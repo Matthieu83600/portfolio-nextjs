b42a574160656f17168f4ae9d83ae4b674fe48f9
Matthieu Bonjour <matthieu83600-pro@outlook.fr>
2025-04-14 21:37:47 +0200

    style: timeline
    fix: error lint

--- components/TimelineSection/Timeline.tsx
+++ components/TimelineSection/Timeline.tsx
@@ -1,6 +1,8 @@
 'use client';
+import * as React from 'react';
 import { useTranslations } from 'next-intl';
 import { useEffect, useRef, useMemo } from 'react';
+import { ScrollArea, ScrollBar } from '@/components/ui/scroll-area';
 
 const TimeLine = () => {
   const carouselRef = useRef<HTMLUListElement>(null);
@@ -9,26 +11,47 @@ const TimeLine = () => {
   const TimeLineData = useMemo(
     () => [
       {
-        year: '2024',
+        id: '1',
+        year: '2025',
         text: t('TimelineSection.2024.text1'),
         default: true, // Cet élément sera visible en premier
       },
+
       {
+        id: '2',
+        year: "2024 - Aujourd'hui",
+        text: t('TimelineSection.2024.text2'),
+      },
+      {
+        id: '3',
         year: '2024',
         text: t('TimelineSection.2024.text3'),
       },
       {
-        year: '2024',
-        text: t('TimelineSection.2024.text2'),
-      },
-      {
+        id: '4',
         year: '2023',
         text: t('TimelineSection.2023.text1'),
       },
-      { year: '2023-2019', text: t('TimelineSection.2023-2019.text1') },
-      { year: '2019', text: t('TimelineSection.2019.text1') },
-      { year: '2018-2014', text: t('TimelineSection.2018-2014.text1') },
-      { year: '2014-2010', text: t('TimelineSection.2014-2010.text1') },
+      {
+        id: '5',
+        year: '2023-2019',
+        text: t('TimelineSection.2023-2019.text1'),
+      },
+      {
+        id: '6',
+        year: '2019',
+        text: t('TimelineSection.2019.text1'),
+      },
+      {
+        id: '7',
+        year: '2018-2014',
+        text: t('TimelineSection.2018-2014.text1'),
+      },
+      {
+        id: '8',
+        year: '2014-2010',
+        text: t('TimelineSection.2014-2010.text1'),
+      },
     ],
     [t]
   );
@@ -48,28 +71,25 @@ const TimeLine = () => {
   }, [TimeLineData]);
 
   return (
-    <section id="timeline" className="mt-5 -mb-5">
-      <ul
-        ref={carouselRef}
-        className="scrollbar-thin scrollbar-track-transparent scrollbar-thumb-light-bg-btn dark:scrollbar-thumb-dark-bg-btn flex snap-x snap-mandatory gap-5 overflow-x-auto py-5"
-      >
-        {TimeLineData.map((item, index) => (
-          <li
-            id={`carousel__item-${index}`}
-            key={index}
-            className="flex min-w-[250px] snap-start flex-col gap-3 hover:cursor-default sm:w-1/3 md:w-1/6"
-          >
-            <h3
-              aria-label={"Ce que j'ai fait en " + item.year}
-              className="flex items-center gap-4 text-2xl font-bold"
+    <section id="timeline" className="mt-10 mb-5 md:mt-5">
+      <ScrollArea className="rounded-md border">
+        <div className="flex space-x-4 p-4">
+          {TimeLineData.map((data) => (
+            <div
+              key={data.id}
+              className="flex w-[20%] shrink-0 flex-col items-center gap-2"
             >
-              {`${item.year}`}
-              <hr className="border-light-bg-btn dark:border-dark-bg-btn w-[58%] border-2" />
-            </h3>
-            <p className="tracking-wide">{item.text}</p>
-          </li>
-        ))}
-      </ul>
+              <h3 className="text-foreground pt-2 text-lg font-semibold">
+                {data.year}
+              </h3>
+              <span className="text-muted-foreground font-semibold">
+                {data.text}
+              </span>
+            </div>
+          ))}
+        </div>
+        <ScrollBar orientation="horizontal" />
+      </ScrollArea>
     </section>
   );
 };
