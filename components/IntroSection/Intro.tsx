238d64548aad48385e89e517d3327b9bdcf5f47d
Matthieu Bonjour <matthieu83600-pro@outlook.fr>
2025-04-14 19:34:44 +0200

    style: intro

--- components/IntroSection/Intro.tsx
+++ components/IntroSection/Intro.tsx
@@ -2,6 +2,7 @@
 import { useTranslations } from 'next-intl';
 import Link from 'next/link';
 import { useScrollTo } from '../../hooks/useScrollTo';
+import { Button } from '../ui/button';
 
 const IntroSection = () => {
   const t = useTranslations('HomePage');
@@ -19,22 +20,25 @@ const IntroSection = () => {
             {t('IntroSection.description')}
           </p>
           <div className="flex flex-col gap-10 sm:flex-row">
-            <Link
-              href="#projects"
-              onClick={onClick}
-              className="bg-light-bg-btn hover:bg-light-bg-btn-hover dark:bg-dark-bg-btn dark:hover:bg-dark-bg-btn-hover rounded-lg border border-black px-7 py-3 text-[14pt] dark:border-white"
-              aria-label="Derniers projets"
-            >
-              {t('IntroSection.linkProjects')}
-            </Link>
-            <Link
-              href="https://cvdesignr.com/p/65770bab74d9f"
-              target="_blank"
-              className="bg-light-bg-btn hover:bg-light-bg-btn-hover dark:bg-dark-bg-btn dark:hover:bg-dark-bg-btn-hover rounded-lg border border-black px-7 py-3 text-[14pt] dark:border-white"
-              aria-label="Mon CV"
-            >
-              {t('IntroSection.linkCV')}
-            </Link>
+            <Button variant="primary" size="perso">
+              <Link
+                href="#projects"
+                onClick={onClick}
+                aria-label="Derniers projets"
+              >
+                {t('IntroSection.linkProjects')}
+              </Link>
+            </Button>
+            <Button variant="primary" size="perso">
+              <Link
+                href="https://cvdesignr.com/p/65770bab74d9f"
+                target="_blank"
+                rel="noopener noreferrer"
+                aria-label="Mon CV"
+              >
+                {t('IntroSection.linkCV')}
+              </Link>
+            </Button>
           </div>
         </div>
       </div>
