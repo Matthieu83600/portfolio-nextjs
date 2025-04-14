cac3e8998755894d1e0a53f6fb9d13a4e9cc7a7f
Matthieu Bonjour <matthieu83600-pro@outlook.fr>
2025-04-14 16:51:37 +0200

    fix: z-index header

--- components/HeaderSection/Header.tsx
+++ components/HeaderSection/Header.tsx
@@ -8,7 +8,7 @@ const Header = ({ locale }: { locale: string }) => {
   const isMobile = useMediaQuery();
 
   return (
-    <header className="sticky top-3 z-10 rounded-xl bg-inherit px-5 py-5 shadow-2xl">
+    <header className="sticky top-3 z-30 rounded-xl bg-inherit px-5 py-5 shadow-2xl">
       <div className="container-md flex justify-between">
         <Logo locale={locale} />
         <div className="flex items-center gap-5">
