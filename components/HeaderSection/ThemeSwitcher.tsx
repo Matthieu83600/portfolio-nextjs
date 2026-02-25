6233895afea1d48abbf103650e7f529d545be156
Matthieu Bonjour <matthieu83600-pro@outlook.fr>
2026-02-25 12:38:06 +0100

    chore: replace icons lucide-react

--- components/HeaderSection/ThemeSwitcher.tsx
+++ components/HeaderSection/ThemeSwitcher.tsx
@@ -1,7 +1,6 @@
 'use client';
 
-import * as React from 'react';
-import { Moon, Sun } from 'lucide-react';
+import { TbMoon, TbSun } from 'react-icons/tb';
 import { useTheme } from 'next-themes';
 import { useTranslations } from 'next-intl';
 import { Button } from '@/components/ui/button';
@@ -20,8 +19,8 @@ const ThemeSwitcher = () => {
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
         <Button variant="outline" size="icon">
-          <Sun className="h-[1.2rem] w-[1.2rem] scale-100 rotate-0 transition-all dark:scale-0 dark:-rotate-90" />
-          <Moon className="absolute h-[1.2rem] w-[1.2rem] scale-0 rotate-90 transition-all dark:scale-100 dark:rotate-0" />
+          <TbSun className="h-[1.2rem] w-[1.2rem] scale-100 rotate-0 transition-all dark:scale-0 dark:-rotate-90" />
+          <TbMoon className="absolute h-[1.2rem] w-[1.2rem] scale-0 rotate-90 transition-all dark:scale-100 dark:rotate-0" />
           <span className="sr-only">Changer de thème</span>
         </Button>
       </DropdownMenuTrigger>
