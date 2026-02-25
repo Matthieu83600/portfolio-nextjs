c89e5c863f0cc51a38673c5d89574c436a53fced
Matthieu Bonjour <matthieu83600-pro@outlook.fr>
2026-02-25 12:34:50 +0100

    chore: replace depreciate icons lucide-react

--- constants/social_media.ts
+++ constants/social_media.ts
@@ -1,28 +1,28 @@
 import { SocialMediaOption } from '@/types/socialMediaOption.types';
-import { Github, Gitlab, Linkedin, Twitter } from 'lucide-react';
+import { TbBrandGithub, TbBrandGitlab, TbBrandLinkedin, TbBrandTwitter } from 'react-icons/tb';
 
 export const SOCIAL_MEDIA: SocialMediaOption[] = [
   {
     id: 'linkedin',
-    icon: Linkedin, // Pas d'élément JSX, mais la référence du composant
+    icon: TbBrandLinkedin, // Pas d'élément JSX, mais la référence du composant
     title: 'linkedin',
     url: 'https://www.linkedin.com/in/matthieu-bonjour',
   },
   {
     id: 'twitter',
-    icon: Twitter,
+    icon: TbBrandTwitter,
     title: 'twitter',
     url: 'https://twitter.com/BonjourMat83600?=V9bTy94aQMLvl_l96_nnZA&s=09',
   },
   {
     id: 'gitlab',
-    icon: Gitlab,
+    icon: TbBrandGitlab,
     title: 'gitlab',
     url: 'https://gitlab.com/Matthieu83600',
   },
   {
     id: 'github',
-    icon: Github,
+    icon: TbBrandGithub,
     title: 'github',
     url: 'https://github.com/Matthieu83600',
   },
